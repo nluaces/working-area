03b30cb6341b38bafb0b8c1b89f5732ec8f7998f
Noe Luaces <nluaces@redhat.com>
2024-01-03 13:09:19 +0100

    update scritps

--- 03-kind-setup-private1.sh
+++ 03-kind-setup-private1.sh
@@ -9,7 +9,5 @@ kubectl config set-context --current --namespace private1
 skupper init --site-name site3 --ingress none --console-auth unsecured
 skupper link create ~/public1-token.yaml
 skupper link create ~/public2-token.yaml 
-kubectl apply -f workspace/hipster-demo/skupper-example-grpc/deployment-ms-a.yaml 
-skupper expose deployment productcatalogservice --address productcatalogservice --port 5001  --protocol http2 
 
 exit 1
--- 03-minikube-setup-private1.sh
+++ 03-minikube-setup-private1.sh
@@ -8,7 +8,7 @@ kubectl config set-context --current --namespace private1
 skupper init --site-name site3 --enable-flow-collector --enable-console --console-user admin --console-password 1234 --enable-cluster-permissions --routers 3
 skupper link create ~/public1-token.yaml
 skupper link create ~/public2-token.yaml 
-kubectl apply -f ~/scripts/deployment-ms-a.yaml 
+kubectl apply -f deployment-ms-a.yaml
 skupper expose deployment productcatalogservice --address productcatalogservice --port 5001,5002  --protocol http2 --generate-tls-secrets --publish-not-ready-addresses
 
 exit 1
